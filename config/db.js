a555f883ed16f40503efa825696eb7285392900b
nish2234 <nishant2003official@gmail.com>
2023-11-10 15:10:20 +0530

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL , {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+      autoIndex: false, // Don't build indexes
+      maxPoolSize: 10, // Maintain up to 10 socket connections
+      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+      family: 4 // Use IPv4, skip trying IPv6
+  
+      });
+    console.log(
+      `Connected to Mongodb Database ${mongoose.connection.host}`.bgMagenta
+        .white
+    );
+  } catch (error) {
+    console.log(`MONGO Connect Error ${error}`.bgRed.white);
+  }
+};
+
+module.exports = connectDB;
